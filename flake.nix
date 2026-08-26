c51fc97fe7d0d86a22127f79065a9a9a7cb979fe
plan9better <plan9better@proton.me>
2026-08-26 19:58:10 +0200

    feat(nix): build with nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,35 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    naersk,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {inherit system;};
+        naersk' = pkgs.callPackage naersk {};
+        src = pkgs.lib.fileset.toSource {
+          root = ./.;
+          fileset = pkgs.lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            ./src
+          ];
+        };
       in {
-        packages = {
-          # derivation
+        packages.default = naersk'.buildPackage {
+          inherit src;
+          pname = "aws-google-oidc";
+          version = "0.1.0";
         };
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [rust-analyzer rustfmt cargo rustc clippy];
